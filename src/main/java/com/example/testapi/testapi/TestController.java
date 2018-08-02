c3b5f55a4aba5da400914fb18e9729f6bd53c011
maguoliang <496719266@qq.com>
2018-08-02 13:14:29 +0800

    111

--- src/main/java/com/example/testapi/testapi/TestController.java
+++ src/main/java/com/example/testapi/testapi/TestController.java
@@ -1,4 +1,18 @@
 package com.example.testapi.testapi;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+@RequestMapping("/api")
 public class TestController {
+
+    @RequestMapping("/test")
+    @ResponseBody
+    public String loadSay() {
+
+        System.out.println("hello world");
+        return "hello world";
+    }
 }
