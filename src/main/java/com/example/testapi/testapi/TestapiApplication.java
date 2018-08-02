7790b0ca860193a46a4f6fda5182585e60a2acec
maguoliang <496719266@qq.com>
2018-08-02 10:23:31 +0800

    1

---
+++ src/main/java/com/example/testapi/testapi/TestapiApplication.java
@@ -0,0 +1,12 @@
+package com.example.testapi.testapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestapiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TestapiApplication.class, args);
+	}
+}
